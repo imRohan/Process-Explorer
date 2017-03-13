2eac6837cf82d055ed1f50dfb4c6b02e468aacc1
Rohan Likhite <rlikhite@corp.entertechsystems.com>
2017-03-13 11:34:49 -0400

    format as JSON and print to console

--- process-agent.go
+++ process-agent.go
@@ -1,25 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/imRohan/go-ps"
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
 	"github.com/satori/go.uuid"
 	"log"
+	"net"
+	"os/user"
 	"strconv"
 	"strings"
-	"time"
-  "os/user"
-  "net"
 )
 
 type Process struct {
-	name      string
-  createdAt time.Time
-	pid       int
-	ppid      int
-	uuid      uuid.UUID
+	Name      string
+	CreatedAt string
+	Pid       int
+	Ppid      int
+	Uuid      uuid.UUID
 }
 
 func getProcesses(defaultProcesses bool, searchString string) []Process {
@@ -32,14 +32,14 @@ func getProcesses(defaultProcesses bool, searchString string) []Process {
 	var output []Process
 
 	for _, process := range processes {
-    createdAt := process.CreationTime()
+		createdAt := process.CreationTime()
 		name := strings.Split(process.Executable(), ".exe")[0]
 		pid := process.Pid()
 		ppid := process.PPid()
 		uuid := uuid.NewV1()
 		if !defaultProcesses || defaultProcesses && createdAt.Year() != -0001 {
 			if len(searchString) == 0 || len(searchString) != 0 && name == searchString {
-				currentProcess := Process{name, createdAt, pid, ppid, uuid}
+				currentProcess := Process{name, createdAt.String(), pid, ppid, uuid}
 				output = append(output, currentProcess)
 			}
 		}
@@ -48,13 +48,21 @@ func getProcesses(defaultProcesses bool, searchString string) []Process {
 	return output
 }
 
+func renderJSON(returnedProcesses []Process) {
+	json, err := json.Marshal(returnedProcesses)
+	if err != nil {
+		fmt.Println("Cannot create JSON ", err)
+	}
+
+	fmt.Println(string(json))
+}
+
 func outputToProcessWindow(processWindow *walk.TextEdit, returnedProcesses []Process) {
-	fmt.Println(returnedProcesses)
 	for _, singleProcess := range returnedProcesses {
-		createdAt := singleProcess.createdAt
-		name := singleProcess.name
-		pid := singleProcess.pid
-		uuid := singleProcess.uuid
+		createdAt := singleProcess.CreatedAt
+		name := singleProcess.Name
+		pid := singleProcess.Pid
+		uuid := singleProcess.Uuid
 		outputString := fmt.Sprintf("%s - %s (%d) [%s] \r", createdAt, name, pid, uuid)
 		for _, applicationString := range strings.Split(outputString, "\n") {
 			processWindow.AppendText(applicationString + "\r\n")
@@ -63,26 +71,26 @@ func outputToProcessWindow(processWindow *walk.TextEdit, returnedProcesses []Pro
 }
 
 func getCurrentUser() string {
-  user, err := user.Current()
+	user, err := user.Current()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-  username := fmt.Sprintf("%s", user.Username)
-  return username
+	username := fmt.Sprintf("%s", user.Username)
+	return username
 }
 
 func getMacAddress() string {
-  interfaces, _ := net.Interfaces()
-  macAddress := ""
-  for _, singleInterface := range interfaces{
-    hardwareName    := singleInterface.Name
-    if hardwareName == "Ethernet" || hardwareName == "ethernet"{
-      macAddress = singleInterface.HardwareAddr.String()
-    }
-  }
-  return macAddress
+	interfaces, _ := net.Interfaces()
+	macAddress := ""
+	for _, singleInterface := range interfaces {
+		hardwareName := singleInterface.Name
+		if hardwareName == "Ethernet" || hardwareName == "ethernet" {
+			macAddress = singleInterface.HardwareAddr.String()
+		}
+	}
+	return macAddress
 }
 
 func main() {
@@ -91,13 +99,12 @@ func main() {
 	var toggleDefaultsCheckBox *walk.CheckBox
 	showDefaultProcesses := false
 	searchFieldString := ""
-  currentUser := getCurrentUser()
-  macAddress := getMacAddress()
-  windowTitle := fmt.Sprintf("%s - %s", currentUser, macAddress)
-
+	currentUser := getCurrentUser()
+	macAddress := getMacAddress()
+	windowTitle := fmt.Sprintf("%s - %s", currentUser, macAddress)
 
 	MainWindow{
-		Title: windowTitle,
+		Title:   windowTitle,
 		MinSize: Size{300, 600},
 		Layout:  VBox{},
 		Children: []Widget{
@@ -142,6 +149,7 @@ func main() {
 							processWindow.SetText("")
 							returnedProcesses := getProcesses(showDefaultProcesses, "")
 							outputToProcessWindow(processWindow, returnedProcesses)
+							renderJSON(returnedProcesses)
 						},
 					},
 				},
