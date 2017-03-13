de5a78aecf1780d6084c80aca98a988d172cf687
Rohan Likhite <rlikhite@corp.entertechsystems.com>
2017-03-13 10:19:31 -0400

    lots of changes, removed duration

--- process-agent.go
+++ process-agent.go
@@ -10,18 +10,16 @@ import (
 	"strconv"
 	"strings"
 	"time"
-)
-
-const (
-	defaultProcessDuration = "2562047h47m16.854775807s"
+  "os/user"
+  "net"
 )
 
 type Process struct {
-	name     string
-	duration time.Duration
-	pid      int
-	ppid     int
-	uuid     uuid.UUID
+	name      string
+  createdAt time.Time
+	pid       int
+	ppid      int
+	uuid      uuid.UUID
 }
 
 func getProcesses(defaultProcesses bool, searchString string) []Process {
@@ -34,14 +32,14 @@ func getProcesses(defaultProcesses bool, searchString string) []Process {
 	var output []Process
 
 	for _, process := range processes {
-		duration := processDuration(process)
+    createdAt := process.CreationTime()
 		name := strings.Split(process.Executable(), ".exe")[0]
 		pid := process.Pid()
 		ppid := process.PPid()
 		uuid := uuid.NewV1()
-		if !defaultProcesses || defaultProcesses && duration.String() != defaultProcessDuration {
+		if !defaultProcesses || defaultProcesses && createdAt.Year() != -0001 {
 			if len(searchString) == 0 || len(searchString) != 0 && name == searchString {
-				currentProcess := Process{name, duration, pid, ppid, uuid}
+				currentProcess := Process{name, createdAt, pid, ppid, uuid}
 				output = append(output, currentProcess)
 			}
 		}
@@ -50,49 +48,41 @@ func getProcesses(defaultProcesses bool, searchString string) []Process {
 	return output
 }
 
-func processDuration(process ps.Process) time.Duration {
-	_processCreationTime := process.CreationTime()
-	_duration := time.Since(_processCreationTime)
-
-	return _duration
-}
-
 func outputToProcessWindow(processWindow *walk.TextEdit, returnedProcesses []Process) {
 	fmt.Println(returnedProcesses)
 	for _, singleProcess := range returnedProcesses {
+		createdAt := singleProcess.createdAt
 		name := singleProcess.name
-		duration := singleProcess.duration
-		timeString := durationSplit(duration)
 		pid := singleProcess.pid
 		uuid := singleProcess.uuid
-		outputString := fmt.Sprintf("%s - %s (%d) [%s] \r", timeString, name, pid, uuid)
+		outputString := fmt.Sprintf("%s - %s (%d) [%s] \r", createdAt, name, pid, uuid)
 		for _, applicationString := range strings.Split(outputString, "\n") {
 			processWindow.AppendText(applicationString + "\r\n")
 		}
 	}
 }
 
-func durationSplit(duration time.Duration) string {
-	durationString := duration.String()
-	splitM := strings.Split(durationString, "m")
-	splitH := strings.Split(string(splitM[0]), "h")
-	output := ""
-	if len(splitH) == 2 {
-		h := string(splitH[0])
-		m := string(splitH[1])
-		s := string(splitM[1])
-		output = fmt.Sprintf("%s:%s:%s", h, m, s[0:2])
-	} else {
-		if len(splitM) == 2 {
-			m := string(splitH[0])
-			s := string(splitM[1])
-			output = fmt.Sprintf("00:%s:%s", m, s[0:2])
-		} else {
-			s := string(splitM[0])
-			output = fmt.Sprintf("00:00:%s", s[0:1])
-		}
+func getCurrentUser() string {
+  user, err := user.Current()
+
+	if err != nil {
+		log.Fatal(err)
 	}
-	return output
+
+  username := fmt.Sprintf("%s", user.Username)
+  return username
+}
+
+func getMacAddress() string {
+  interfaces, _ := net.Interfaces()
+  macAddress := ""
+  for _, singleInterface := range interfaces{
+    hardwareName    := singleInterface.Name
+    if hardwareName == "Ethernet" || hardwareName == "ethernet"{
+      macAddress = singleInterface.HardwareAddr.String()
+    }
+  }
+  return macAddress
 }
 
 func main() {
@@ -101,9 +91,13 @@ func main() {
 	var toggleDefaultsCheckBox *walk.CheckBox
 	showDefaultProcesses := false
 	searchFieldString := ""
+  currentUser := getCurrentUser()
+  macAddress := getMacAddress()
+  windowTitle := fmt.Sprintf("%s - %s", currentUser, macAddress)
+
 
 	MainWindow{
-		Title:   "Go Look At Processes!",
+		Title: windowTitle,
 		MinSize: Size{300, 600},
 		Layout:  VBox{},
 		Children: []Widget{
