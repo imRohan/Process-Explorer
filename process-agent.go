3342140e39c87246dc4d558e3cf2a8271f8df2e6
Rohan Likhite <rlikhite@corp.entertechsystems.com>
2017-03-22 15:28:11 -0400

    strip unwanted methods for running as process

--- process-agent.go
+++ process-agent.go
@@ -5,15 +5,18 @@ import (
 	"errors"
 	"fmt"
 	"github.com/imRohan/go-ps"
-	"github.com/lxn/walk"
-	. "github.com/lxn/walk/declarative"
 	"github.com/satori/go.uuid"
+  "github.com/kardianos/service"
 	"net"
+  //"os"
 	"os/user"
-	"strconv"
 	"strings"
 	"time"
 )
+
+var autoRefresh bool = false
+var showDefaultProcesses bool = false
+var logger service.Logger
 
 type Process struct {
 	Name      string    `json:"name"`
@@ -23,15 +26,15 @@ type Process struct {
 	Uuid      uuid.UUID `json:"uuid"`
 }
 
-var autoRefresh bool = false
-var processWindow, searchField *walk.TextEdit
-var autoRefreshCheckbox, toggleDefaultsCheckbox *walk.CheckBox
-var searchFieldString string
-var showDefaultProcesses bool = false
+type outputStruct struct {
+  Username   string    `json:"username"`
+  MacAddress string    `json:"macAddress"`
+  Processes  []Process `json:"processes"`
+}
+
 
 func getProcesses() (output []Process, err error) {
 	defaultProcesses := showDefaultProcesses
-	searchString := searchFieldString
 
 	processes, err := ps.Processes()
 	if err != nil {
@@ -45,10 +48,8 @@ func getProcesses() (output []Process, err error) {
 		ppid := process.PPid()
 		uuid := uuid.NewV1()
 		if !defaultProcesses || defaultProcesses && createdAt.Year() != -0001 {
-			if len(searchString) == 0 || len(searchString) != 0 && name == searchString {
-				currentProcess := Process{name, createdAt.String(), pid, ppid, uuid}
-				output = append(output, currentProcess)
-			}
+      currentProcess := Process{name, createdAt.String(), pid, ppid, uuid}
+      output = append(output, currentProcess)
 		}
 	}
 
@@ -65,11 +66,6 @@ func renderJSON(returnedProcesses []Process) error {
 		return err
 	}
 
-	type outputStruct struct {
-		Username   string    `json:"username"`
-		MacAddress string    `json:"macAddress"`
-		Processes  []Process `json:"processes"`
-	}
 
 	outputPackage := outputStruct{currentUser, macAddress, returnedProcesses}
 
@@ -80,23 +76,6 @@ func renderJSON(returnedProcesses []Process) error {
 
 	fmt.Println(string(json))
 	return nil
-}
-
-func outputToProcessWindow(returnedProcesses []Process) {
-	go renderJSON(returnedProcesses)
-
-	processWindow.SetText("")
-
-	for _, singleProcess := range returnedProcesses {
-		createdAt := singleProcess.CreatedAt
-		name := singleProcess.Name
-		pid := singleProcess.Pid
-		uuid := singleProcess.Uuid
-		outputString := fmt.Sprintf("%s - %s (%d) [%s] \r", createdAt, name, pid, uuid)
-		for _, applicationString := range strings.Split(outputString, "\n") {
-			processWindow.AppendText(applicationString + "\r\n")
-		}
-	}
 }
 
 func getCurrentUser() (username string, err error) {
@@ -131,89 +110,15 @@ func initAutoRefresh() {
 		status := autoRefresh
 		if status {
 			fmt.Println("Refresh Processes")
-			processWindow.SetText("")
-			searchFieldString = searchField.Text()
 			returnedProcesses, err := getProcesses()
 			if err != nil {
 				fmt.Println(err)
 			} else {
-				outputToProcessWindow(returnedProcesses)
+        renderJSON(returnedProcesses)
 			}
 		}
 	}
 }
 
 func main() {
-	go initAutoRefresh()
-
-	MainWindow{
-		Title:   "Process Agent",
-		MinSize: Size{500, 500},
-		Layout:  VBox{},
-		Children: []Widget{
-			HSplitter{
-				Children: []Widget{
-					CheckBox{
-						AssignTo: &autoRefreshCheckbox,
-						Text:     "Auto Refresh",
-						Checked:  false,
-						OnCheckStateChanged: func() {
-							autoRefresh = !autoRefresh
-							checkboxValue := strconv.FormatBool(autoRefresh)
-							checkboxOutput := fmt.Sprintf("Auto Refresh: %s \n", checkboxValue)
-							processWindow.AppendText(checkboxOutput)
-						},
-					},
-					CheckBox{
-						AssignTo: &toggleDefaultsCheckbox,
-						Text:     "Hide Defaults",
-						Checked:  false,
-						OnCheckStateChanged: func() {
-							showDefaultProcesses = !showDefaultProcesses
-							checkboxValue := strconv.FormatBool(showDefaultProcesses)
-							checkboxOutput := fmt.Sprintf("Hide System Processes: %s \n", checkboxValue)
-							processWindow.AppendText(checkboxOutput)
-						},
-					},
-					TextEdit{
-						AssignTo: &searchField,
-					},
-					PushButton{
-						Text: "Filter",
-						OnClicked: func() {
-							searchFieldString = searchField.Text()
-							returnedProcesses, err := getProcesses()
-							if err != nil {
-								fmt.Println(err)
-							} else {
-								outputToProcessWindow(returnedProcesses)
-							}
-						},
-					},
-				},
-			},
-			HSplitter{
-				MinSize: Size{300, 570},
-				Children: []Widget{
-					TextEdit{AssignTo: &processWindow, ReadOnly: true},
-				},
-			},
-			HSplitter{
-				Children: []Widget{
-					PushButton{
-						Text: "Get All Processes",
-						OnClicked: func() {
-							searchFieldString = ""
-							returnedProcesses, err := getProcesses()
-							if err != nil {
-								fmt.Println(err)
-							} else {
-								outputToProcessWindow(returnedProcesses)
-							}
-						},
-					},
-				},
-			},
-		},
-	}.Run()
 }
