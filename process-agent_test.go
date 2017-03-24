fb332324592016824ffb61734b66501e9d033d6f
Rohan Likhite <rlikhite@corp.entertechsystems.com>
2017-03-24 13:11:02 -0400

    removed unnecessary check

--- process-agent_test.go
+++ process-agent_test.go
@@ -4,34 +4,29 @@ import (
 	"testing"
 )
 
-func TestGetProcesses(t *testing.T){
+func TestGetProcesses(t *testing.T) {
 	_, err := getProcesses()
 	if err != nil {
 		t.Error("Could Not Get Processes")
 	}
 }
 
-func TestJSONOutput(t *testing.T){
-	output, err := getProcesses()
-	if err != nil {
-		t.Error("Could Not Get Processes")
-	}
-
-	err = renderJSON(output)
+func TestJSONOutput(t *testing.T) {
+	output,_ := getProcesses()
+  err := renderJSON(output)
 	if err != nil {
 		t.Error("Could Not Render JSON")
 	}
 }
 
-func TestGetUsername(t *testing.T){
+func TestGetUsername(t *testing.T) {
 	_, err := getCurrentUser()
 	if err != nil {
 		t.Error("Could Not Get Current User")
 	}
 }
 
-
-func TestGetMACAddr(t *testing.T){
+func TestGetMACAddr(t *testing.T) {
 	_, err := getMacAddress()
 	if err != nil {
 		t.Error("Could Not Get MacAddress")
