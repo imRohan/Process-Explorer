97063adce197a238c4b21a468bb4710c1ff91687
Rohan Likhite <rlikhite@corp.entertechsystems.com>
2017-03-29 10:53:56 -0400

    fixed broken tests now that userdetails is a struct, not a global var

--- process-agent_test.go
+++ process-agent_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 )
 
+var test_user UserDetails
+
 func TestGetProcesses(t *testing.T) {
 	_, err := getProcesses()
 	if err != nil {
@@ -11,24 +13,29 @@ func TestGetProcesses(t *testing.T) {
 	}
 }
 
+func TestGetUsername(t *testing.T) {
+	name, err := getCurrentUser()
+	if err != nil {
+		t.Error("Could Not Get Current User")
+	}
+  test_user.Name = name
+}
+
+func TestGetMACAddr(t *testing.T) {
+	mac, err := getMacAddress()
+	if err != nil {
+		t.Error("Could Not Get MacAddress")
+	}
+
+  test_user.Mac = mac
+}
+
 func TestJSONOutput(t *testing.T) {
 	output,_ := getProcesses()
-  err := renderJSON(output)
+
+  err := renderJSON(test_user, output)
 	if err != nil {
 		t.Error("Could Not Render JSON")
 	}
 }
 
-func TestGetUsername(t *testing.T) {
-	_, err := getCurrentUser()
-	if err != nil {
-		t.Error("Could Not Get Current User")
-	}
-}
-
-func TestGetMACAddr(t *testing.T) {
-	_, err := getMacAddress()
-	if err != nil {
-		t.Error("Could Not Get MacAddress")
-	}
-}
